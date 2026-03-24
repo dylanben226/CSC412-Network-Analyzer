4356c03699a637359fc794e35731cf222fe867af
dylanben226 <bend@berea.edu>
2026-03-24 09:22:42 -0400

    Updated code

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,12 @@
-def capture_packets(duration):    pass
-def count_protocols(packets):    pass
-def print_results(counts):    pass
+def count_protocols(packets):
+    counts = {"TCP": 0, "UDP": 0, "ICMP": 0, "Other": 0}
+    for packet in packets:
+        if packet.haslayer("TCP"):
+            counts["TCP"] += 1
+        elif packet.haslayer("UDP"):
+            counts["UDP"] += 1
+        elif packet.haslayer("ICMP"):
+            counts["ICMP"] += 1
+        else:
+            counts["Other"] += 1
+    return counts
